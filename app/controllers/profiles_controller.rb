c82fc642df2f88e4d2fede69b1cffcf0e70893a7
likwokbiu <li_bill@ymail.com>
2018-02-21 15:18:00 -0500

    Completed

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -1,4 +1,14 @@
 class ProfilesController < ApplicationController
+
   def new
+    @form_keywords = [
+      "email",
+      "username",
+      "pin",
+      "website",
+      "address",
+      "alias"
+    ]
   end
+
 end
